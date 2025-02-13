ab9c2c4a0efcbf6858e8d408b02ad42ac9a942c7
Jeremy <wjgrice@gmail.com>
2025-02-12 22:39:47 -0600

    Updated chatbot url for to point to amplify app.

--- src/components/ChatbotCard.jsx
+++ src/components/ChatbotCard.jsx
@@ -21,7 +21,7 @@ const ChatbotCard = () => {
     setAudioLoading(false);
     try {
       const response = await fetch(
-        'https://gptassist-fastapi-env.eba-62mwwktp.us-east-1.elasticbeanstalk.com/api/chat/',
+        'https://chatbot.jeremygrice.tech/api/chat/api/chat/',
         {
           method: 'POST',
           headers: {
@@ -49,7 +49,7 @@ const ChatbotCard = () => {
     setAudioLoading(true);
     try {
       const response = await fetch(
-        'https://gptassist-fastapi-env.eba-62mwwktp.us-east-1.elasticbeanstalk.com/api/chat/audio/',
+        'https://chatbot.jeremygrice.tech/api/chat/api/chat/audio/',
         {
           method: 'POST',
           headers: {
